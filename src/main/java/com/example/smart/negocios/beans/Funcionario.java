914a0430d7851f1aea326a37578c8aea0fb39225
Artur Guimarães <arturguimaraes82@gmail.com>
2022-09-01 12:39:25 -0300

    Funcionario
    implementação da classe Funcionario

---
+++ src/main/java/com/example/smart/negocios/beans/Funcionario.java
@@ -0,0 +1,30 @@
+package com.example.smart.negocios.beans;
+
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa{
+    private double salario;
+    private String endereco;
+
+    public Funcionario(String nome, String cpf, String telefone, String email, LocalDate dataNascimento, double salario, String endereco) {
+        super(nome, cpf, telefone, email, dataNascimento);
+        this.salario = salario;
+        this.endereco = endereco;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
