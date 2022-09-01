770e12a1f6cad20dc144f42372875fdd3bb2071f
Artur Guimarães <arturguimaraes82@gmail.com>
2022-09-01 12:33:58 -0300

    método calcular idade
    calculando método calcular idade em Pessoa

--- src/main/java/com/example/smart/negocios/beans/Pessoa.java
+++ src/main/java/com/example/smart/negocios/beans/Pessoa.java
@@ -56,4 +56,19 @@ public class Pessoa {
     public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
+    public int calcularIdade(){
+        LocalDate dataHj = LocalDate.now();
+
+        int idade = dataHj.getYear() - dataNascimento.getYear();
+
+        if (dataHj.getMonthValue() < dataNascimento.getMonthValue())
+            idade = idade - 1;
+        else if(dataHj.getMonthValue() == dataNascimento.getMonthValue()) {
+            if(dataHj.getDayOfMonth() < dataNascimento.getDayOfMonth()) {
+                idade -= 1;
+            }
+        }
+        return idade;
+    }
 }
