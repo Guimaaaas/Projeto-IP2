21219c3f53274bf15c03cb846f5bd38d55fc5c9a
James <james.nascimento@ufrpe.br>
2022-09-02 14:38:01 -0300

    Implementação da Classe Gerente e do método verificarValidade na Classe Alimento.

--- src/main/java/com/smart/negocios/beans/Alimento.java
+++ src/main/java/com/smart/negocios/beans/Alimento.java
@@ -1,6 +1,7 @@
 package com.smart.negocios.beans;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Alimento extends Produto{
     private LocalDate dataValidade;
@@ -9,12 +10,28 @@ public class Alimento extends Produto{
         super(nome, marca, preco);
         this.dataValidade = dataValidade;
     }
-
     public LocalDate getDataValidade() {
         return dataValidade;
     }
 
     public void setDataValidade(LocalDate dataValidade) {
         this.dataValidade = dataValidade;
+    }
+
+    public boolean verificarValidade(){
+
+        boolean validadeOk = false;
+
+        LocalDate dataDeValidade = this.dataValidade;
+        LocalDate hoje = LocalDate.now();
+        Period periodo = Period.between(hoje, dataDeValidade);
+        //System.out.printf("%s", periodo.getDays());
+        int dias = periodo.getDays();
+
+        if(dias >= 0){
+
+            validadeOk = true;
+        }
+        return validadeOk;
     }
 }
